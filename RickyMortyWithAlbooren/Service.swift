eecddd3670dff925e059f4c9b3f093031ed53c20
alihanozaltin <alihanozaltin@gmail.com>
2023-03-08 13:58:49 +0300

    Model and Service

--- RickyMortyWithAlbooren/Service.swift
+++ RickyMortyWithAlbooren/Service.swift
@@ -5,4 +5,31 @@
 //  Created by Alihan Özaltın on 7.03.2023.
 //
 
-import Foundation
+import Alamofire
+
+enum RickyMortyServiceEndPoint: String {
+    case BASE_URL = "https://rickandmortyapi.com/api"
+    case PATH = "/character"
+  
+    static func characterPath() -> String {
+       return "\(BASE_URL.rawValue)\(PATH.rawValue)"
+    }
+}
+
+protocol IRickyMortiyeService {
+    func fetchAllDatas(response:@escaping ([Result]?) -> Void)
+    
+}
+
+struct RickyMortyService: IRickyMortiyeService {
+    func fetchAllDatas(response:@escaping ([Result]?) -> Void) {
+        AF.request(RickyMortyServiceEndPoint.characterPath()).responseDecodable(of: PostModel.self){ (model) in
+            guard let data = model.value else {
+                // error
+                response(nil)
+                return
+            }
+            response(data.results)
+        }
+    }
+}
