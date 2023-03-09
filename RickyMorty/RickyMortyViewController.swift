7cad1c11bba631dae7e4d8249bfcee2710289155
alihanozaltin <alihanozaltin@gmail.com>
2023-03-09 13:48:25 +0300

    home UI done

--- RickyMorty/RickyMortyViewController.swift
+++ RickyMorty/RickyMortyViewController.swift
@@ -15,23 +15,41 @@ protocol RickyMortyOutPut {
 }
 
 
- class RickyMortyViewController: UIViewController {
+class RickyMortyViewController: UIViewController, RickyMortyOutPut {
+    func changeLoading(isLoad: Bool) {
+        isLoad ? indicator.startAnimating() : indicator.stopAnimating()
+    }
+    
+    func saveDatas(values: [Result]) {
+        results = values
+        self.tableView.reloadData()
+    
+    }
+    
      let labelTitle : UILabel = UILabel()
      let indicator : UIActivityIndicatorView = UIActivityIndicatorView()
-     let box : UIView = UIView()
+     let tableView : UITableView = UITableView()
     
+     private lazy var results: [Result] = []
+    
+     lazy var viewModel: IRickyMortyViewModel = RickyMortyViewModel()
+     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
-    
+        viewModel.setDelegate(output: self)
+        viewModel.fetchItems()
     }
+    
     private func configure() {
         view.addSubview(labelTitle)
-        view.addSubview(box)
+        view.addSubview(tableView)
         view.addSubview(indicator)
-        box.backgroundColor = .red
+        tableView.backgroundColor = .clear
+        tableView.delegate = self
+        tableView.dataSource = self
         view.backgroundColor = .white
         labelTitle.text = "Ricky Morty"
         indicator.color = .red
@@ -43,7 +61,7 @@ protocol RickyMortyOutPut {
             make.right.equalTo(view).offset(-10)
             make.height.greaterThanOrEqualTo(10)
         }
-        box.snp.makeConstraints { make in
+        tableView.snp.makeConstraints { make in
             make.top.equalTo(labelTitle.snp.bottom).offset(5)
             make.bottom.equalToSuperview()
             make.left.right.equalTo(labelTitle)
@@ -55,6 +73,16 @@ protocol RickyMortyOutPut {
         }
     }
 
+}
 
+extension RickyMortyViewController: UITableViewDelegate , UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = results[indexPath.row].name ?? ""
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
 }
 
---
+++ RickyMorty/RickyMortyViewControllerCell.swift
@@ -0,0 +1,16 @@
+//
+//  RickyMortyViewControllerCell.swift
+//  RickyMorty
+//
+//  Created by Alihan Özaltın on 9.03.2023.
+//
+
+import UIKit
+
+class RickyMortyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
